824fab979b81c08643fb752f925f5be8c218518d
fynks <75840152+fynks@users.noreply.github.com>
2025-08-27 18:44:33 +0500

    update caching configuration to target repodash.pages.dev

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   runtimeCaching: [
     {
-      // Cache images only for repodash.netlify.app with Cache First (30 days)
-      urlPattern: new RegExp('^https?://repodash\\.netlify\\.app/.*\\.(?:png|jpe?g|gif|webp|svg|ico)$', 'i'),
+      // Cache images only for repodash.pages.dev with Cache First (30 days)
+      urlPattern: new RegExp('^https?://repodash\\.pages\\.dev/.*\\.(?:png|jpe?g|gif|webp|svg|ico)$', 'i'),
       handler: 'CacheFirst',
       options: {
         cacheName: 'images-cache',
@@ -13,8 +13,8 @@ module.exports = {
       },
     },
     {
-      // Cache HTML, CSS, JS only for repodash.netlify.app with Stale While Revalidate (1 day)
-      urlPattern: new RegExp('^https?://repodash\\.netlify\\.app/.*\\.(?:js|css|html)$', 'i'),
+      // Cache HTML, CSS, JS only for repodash.pages.dev with Stale While Revalidate (1 day)
+      urlPattern: new RegExp('^https?://repodash\\.pages\\.dev/.*\\.(?:js|css|html)$', 'i'),
       handler: 'StaleWhileRevalidate',
       options: {
         cacheName: 'static-resources',
